8a03c656ab2a6f83f0c0a6d08363def8b55b7e05
magak <magakk.stepanyan@gmail.com>
2018-01-28 04:25:22 -0800

    controlling speed not acceleration

--- Project3/ros/src/twist_controller/twist_controller.py
+++ Project3/ros/src/twist_controller/twist_controller.py
@@ -25,18 +25,15 @@ class Controller(object):
 	self.wheel_radius = wheel_radius
 
 	self.low_pass_steer_filter = LowPassFilter(0.3, 1.0)
-	self.low_pass_accel_filter = LowPassFilter(0.3, 1.0)
-	self.low_pass_throttle_filter = LowPassFilter(0.3, 1.0)
 
 	self.acceleration_controller  = PID(0.09, 0.00005, 0.007, self.decel_limit, self.accel_limit)
-	self.throttle_controller = PID(0.4, 0.05, 0.0, 0.0, 1.0)
+	self.throttle_controller = PID(1.5, 0.00007, 0.01, 0.0, 0.5)	
 	self.steering_controller = PID(2.0, 0.0005, 0.01, -10.0, 10.0)
 	
 	self.throttle_controller.reset()
 	self.acceleration_controller.reset()
 	
 	self.lastControlTime = None
-	self.lastVelocity = 0.0
 	self.dbw_status = True
 
     def control(self, linear_velocity, angular_velocity, current_velocity, dbw_status):
@@ -51,7 +48,7 @@ class Controller(object):
 			self.steering_controller.reset()
 		self.dbw_status = False
 		return None, None, None
-		
+			
 
 	current_time = time.time()
 	if self.lastControlTime is None:
@@ -64,34 +61,15 @@ class Controller(object):
 	throttle = 0.0
 	brake = 0.0
 
-#	steering = self.yaw_controller.get_steering(abs(linear_velocity), self.low_pass_steer_filter.filt(angular_velocity), current_velocity)
-	steeringInit = self.yaw_controller.get_steering(abs(linear_velocity), angular_velocity, current_velocity)
-	steering = self.low_pass_steer_filter.filt(self.steering_controller.step(steeringInit, sample_time))
-	#rospy.loginfo("init= "+str(steeringInit)+" out= "+str(steering))
+	steering = self.steering_controller.step(self.yaw_controller.get_steering(abs(linear_velocity), self.low_pass_steer_filter.filt(angular_velocity), current_velocity), sample_time)
 
-	acceleration = self.low_pass_accel_filter.filt(self.acceleration_controller.step(linear_velocity-current_velocity, sample_time))
-
-	#rospy.loginfo(str(acceleration))
+	acceleration = self.acceleration_controller.step(linear_velocity-current_velocity, sample_time)	
+		
+	if(acceleration < -self.brake_deadband):
+		brake = (self.vehicle_mass + self.fuel_capacity*GAS_DENSITY)*abs(acceleration)*self.wheel_radius
 	
-	if(acceleration < 0.0):
-		# braking
-		if(abs(acceleration) > self.brake_deadband):
-			brake = (self.vehicle_mass + self.fuel_capacity*GAS_DENSITY)*abs(acceleration)*self.wheel_radius
-			#rospy.loginfo("Braking:")
-		#else:
-			#rospy.loginfo("Brake deadband:")
-	else:
-		# accelerating
-		currentAcceleration =  (current_velocity-self.lastVelocity)/sample_time
-		throttle = self.low_pass_throttle_filter.filt(self.throttle_controller.step(acceleration-currentAcceleration, sample_time))
-		#rospy.loginfo("Throttle:")
+	throttle = self.throttle_controller.step(linear_velocity-current_velocity, sample_time)	
 	
-	self.lastVelocity = current_velocity
-	
-	#throttle = self.throttle_controller.step(linear_velocity-current_velocity, sample_time)
-	#rospy.loginfo("throttle= "+str(throttle)+" brake= "+str(brake)+" steering="+str(steering))
-
-	#rospy.loginfo(str(throttle))
 
 	# Holding
 	if(linear_velocity < STOP_THRESHOLD_VELOCITY and current_velocity < self.min_speed):
